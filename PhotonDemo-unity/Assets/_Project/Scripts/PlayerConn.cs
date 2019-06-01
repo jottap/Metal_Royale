ab9780a0a094f7146fde0b039339aa1b966f1308
Jottap-PC <joaopaulo_oli@hotmail.com>
2019-06-01 11:12:40 -0300

    Add Script manager Player

---
+++ PhotonDemo-unity/Assets/_Project/Scripts/PlayerConn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class PlayerConn : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private PhotonView m_pv;
+
+    [SerializeField]
+    private Canvas m_canvas;
+
+    [SerializeField]
+    private TextMeshProUGUI m_textMeshProUGUI;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_pv = GetComponent<PhotonView>();
+        m_textMeshProUGUI.text = m_pv.Owner.NickName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_canvas.transform.LookAt(Camera.main.transform);
+    }
+}
