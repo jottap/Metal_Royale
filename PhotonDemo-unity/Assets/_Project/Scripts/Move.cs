b564e4ac91f118ea598fc0429f818337f49d80c0
Jottap-PC <joaopaulo_oli@hotmail.com>
2019-06-01 00:21:45 -0300

    Add Script Simple Move

---
+++ PhotonDemo-unity/Assets/_Project/Scripts/Move.cs
@@ -0,0 +1,52 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private PhotonView m_pv;
+
+    [SerializeField]
+    private Vector2 m_dir;
+    [SerializeField]
+    private float m_vel = 2;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_pv = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_pv.IsMine)
+            MoveInput();
+    }
+
+    private void MoveInput()
+    {
+        m_dir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            m_dir += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            m_dir += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            m_dir += Vector2.right;
+        }
+
+        transform.Translate(m_dir * m_vel * Time.deltaTime);
+
+    }
+}
