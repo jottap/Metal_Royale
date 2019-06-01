6709abc2aa31fbf86c7f2bddcce29dd76ce29caa
Jottap-PC <joaopaulo_oli@hotmail.com>
2019-05-31 22:32:38 -0300

    Create Lobby Demo

---
+++ PhotonDemo-unity/Assets/_Project/Scripts/Conn.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class Conn : MonoBehaviourPunCallbacks
+{
+    #region Variables
+
+    [Header("Settings")]
+    [SerializeField]
+    private GameObject m_panelL;
+    [SerializeField]
+    private GameObject m_panelR;
+
+    [SerializeField]
+    private TMP_InputField m_namePlayer;
+    [SerializeField]
+    private TMP_InputField m_nameRoom;
+
+    [SerializeField]
+    private TextMeshProUGUI m_nickName;
+
+    #endregion
+
+    public void Login()
+    {
+        PhotonNetwork.NickName = m_namePlayer.text;
+        PhotonNetwork.ConnectUsingSettings();
+
+        m_panelL.SetActive(false);
+        m_panelR.SetActive(true);
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.JoinOrCreateRoom(m_nameRoom.text, new RoomOptions(), TypedLobby.Default);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PhotonNetwork.Disconnect();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(" Conectado !");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log(" Join Lobby !");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log(" Conexão Perdida !");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log(" Não entrou em nenhuma sala!");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(" Não entrou em nenhuma sala!");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.LogFormat(" Entrei em uma sala !");
+        Debug.LogFormat("Name Room : [{0}] Players Count : [{1}], NickName : [{2}]", PhotonNetwork.CurrentRoom.Name, PhotonNetwork.CurrentRoom.PlayerCount, PhotonNetwork.NickName);
+        m_nickName.text = PhotonNetwork.NickName;
+    }
+
+}
