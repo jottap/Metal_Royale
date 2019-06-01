45824bdf047e3dbf76f46fa27a98e281fe620d6d
Jottap-PC <joaopaulo_oli@hotmail.com>
2019-06-01 12:57:13 -0300

    Add Script MainScene

---
+++ PhotonDemo-unity/Assets/_Project/Scripts/Manager/MainScene.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour
+{
+    #region Variables
+
+    [Header("Settings")]
+    [SerializeField]
+    private GameObject m_panelL;
+    [SerializeField]
+    private GameObject m_panelR;
+    [SerializeField]
+    private GameObject m_panelLoading;
+
+    [SerializeField]
+    private TMP_InputField m_namePlayer;
+    [SerializeField]
+    private TMP_InputField m_nameRoom;
+
+    [SerializeField]
+    private TextMeshProUGUI m_nickName;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_panelL.SetActive(true);
+
+        SubscriveEvent();
+    }
+
+    private void OnDestroy()
+    {
+        UnSubscriveEvent();
+    }
+
+    private void SubscriveEvent()
+    {
+        Conn.Instance.LoginAction += Login;
+    }
+
+    private void UnSubscriveEvent()
+    {
+        Conn.Instance.LoginAction -= Login;
+    }
+
+    public void Login()
+    {
+        m_panelL.SetActive(false);
+        m_panelLoading.SetActive(true);
+    }
+
+}
