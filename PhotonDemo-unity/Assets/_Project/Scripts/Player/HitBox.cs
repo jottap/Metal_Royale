926e124c00ea3f22e31be057cd06c288ad5cbc98
Phillipe Augusto <phillipe.a.a.mendonca@gmail.com>
2019-06-02 00:52:34 -0300

    Implementacao de kick e stun para teste online

---
+++ PhotonDemo-unity/Assets/_Project/Scripts/Player/HitBox.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    [SerializeField] private Collider2D m_OwnPlayerCollider2D;
+    [SerializeField] private PlayerMovement m_OwnPlayerMovement;
+    [SerializeField] private float m_CollisionDuration = 0.4F;
+
+    private float m_CollisionTimer = 0F;
+
+    private void Update()
+    {
+        m_CollisionTimer += Time.deltaTime;
+        if (m_CollisionTimer >= m_CollisionDuration)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        m_CollisionTimer = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (m_OwnPlayerCollider2D != collision)
+        {
+            PlayerSkillSet playerSkillSet = collision.gameObject.GetComponent<PlayerSkillSet>();
+            if (playerSkillSet != null)
+            {
+                playerSkillSet.HitAndStun(m_OwnPlayerMovement.CharacterDirection);
+                playerSkillSet.ReleaseSkillSet();
+            }
+        }
+    }
+}
