f26a55d408c62ec7ee09bf2de43e16b31e9ea400
philippeander <philippeander@gmail.com>
2019-06-03 22:17:28 -0300

    [ADD] ( File: Guittar images ) - ( Branch: PhilippeAnderson ) - ( Dev: philippeander ) - Tela de Menu

---
+++ PhotonDemo-unity/Assets/_Project/Scripts/Menu/RowTextureMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RowTextureMovement : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    [SerializeField] private float m_scrollSpeed = 0.5f;
+    RawImage m_rawImage;
+
+    void Start()
+    {
+        m_rawImage = GetComponent<RawImage>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * m_scrollSpeed;
+        m_rawImage.uvRect = new Rect(0, offset, 1, 1); ;
+    }
+}
